4b015b702cdfec9599a4f71f1f018ce02db6d332
FlooGeR Alex <fridrih.oushen@yandex.ru>
2021-04-29 20:48:53 +0300

    123

---
+++ NewClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TestGit
+{
+    static class NewClass
+    {
+        //Метод, принимающий делегат
+
+        public static void Function(Fun F, double a, double x, double b)
+        {
+
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} | {2,8:0.000} |", a, x, F(a, x));
+                x += 1;
+            }
+
+        }
+
+        //Метод возвращает значение функции a*x^2
+        public static double MyFunc(double a, double x)
+        {
+            return a * x * x;
+        }
+
+        //Метод возвращает значение функции a*sin(x)
+        public static double MySin(double a, double x)
+        {
+            return a * Math.Sin(x);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,12 +6,27 @@ using System.Threading.Tasks;
 
 namespace TestGit
 {
+    //Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double (double, double). 
+    //Продемонстрировать работу на функции с функцией a* x^2 и функцией a* sin(x).
+
+
+    public delegate double Fun(double a, double x);
+
     class Program
     {
-        static void Main(string[] args)
+
+        //123
+        static void Main()
         {
 
+            Console.WriteLine("Таблица функции a*x^2:");
 
+            NewClass.Function(NewClass.MyFunc, -1.5, -2, 2);
+
+            Console.WriteLine("Таблица функции a*sin(x):");
+            NewClass.Function(NewClass.MySin, 3, -2, 2);
+
+            Console.ReadKey();
         }
     }
 }
